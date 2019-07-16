166fb5dbf2311cee2d52cb424122688f3095cbeb
Adegbola Stephen <sfadegbola@student.lautech.edu.ng>
2019-07-16 14:57:16 +0100

    Remove Sign Up Validatio

--- server/middlewares/UserMiddleware.js
+++ server/middlewares/UserMiddleware.js
@@ -12,51 +12,6 @@ function checkPasswordComplexity(pwd) {
 
 /* eslint-disable camelcase */
 export default class UserMiddleware {
-  static async validateSignUp(req, res, next) {
-    try {
-      // eslint-disable-next-line object-curly-newline
-      const { first_name, last_name, email_address, is_admin, password } = req.body;
-      if (!first_name) {
-        throw new APIError(400, 'first_name is required');
-      }
-      if (!last_name) {
-        throw new APIError(400, 'last_name is required');
-      }
-      if (!email_address) {
-        throw new APIError(400, 'email_address is required');
-      }
-      if (!password) {
-        throw new APIError(400, 'password is required');
-      }
-      if (typeof first_name !== 'string') {
-        throw new APIError(400, 'first_name should be a string');
-      }
-      if (typeof last_name !== 'string') {
-        throw new APIError(400, 'last_name should be a string');
-      }
-      const user = await UserServices.findUserByEmail(req.body.email_address);
-      if (typeof email_address !== 'string') {
-        throw new APIError(400, 'email_address should be a string');
-      }
-      if (user.length > 0) {
-        throw new APIError(400, 'email_address already exists');
-      }
-      if (typeof is_admin !== 'boolean') {
-        throw new APIError(400, 'admin should be boolean');
-      }
-      if (typeof password !== 'string') {
-        throw new APIError(400, 'Password must be a field');
-      }
-      if (checkPasswordComplexity(password) === false) {
-        throw new APIError(400, 'passwords only accepts aplha numeric characters');
-      }
-      next();
-    } catch (error) {
-      res.status(error.statusCode || 500).json(
-        new Response(false, error.statusCode || 500, error.message),
-      );
-    }
-  }
 
   static async validateSignIn(req, res, next) {
     try {
--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -4,7 +4,7 @@ import UserMiddleware from '../middlewares/UserMiddleware';
 import TokenMiddleWare from '../middlewares/TokenMiddleware';
 
 const userRouter = express.Router();
-userRouter.post('/auth/signup', UserMiddleware.validateSignUp, UserController.signUp);
+userRouter.post('/auth/signup', UserController.signUp);
 userRouter.post('/auth/signin', UserMiddleware.validateSignIn, UserController.signIn);
 userRouter.post('/createtrip', TokenMiddleWare.validateToken, UserMiddleware.validateCreateTrip, UserController.createTrip);
 userRouter.post('/canceltrip', TokenMiddleWare.validateToken, UserMiddleware.checkIsAdmin, UserController.cancleTrip);
